09d1e4e54058f0178405c6135e0256cf2a6b7b58
mikeyo4800 <mikeyo4800@gmail.com>
2022-08-31 13:26:35 -0400

    updated app

--- tweet_count.py
+++ tweet_count.py
@@ -10,7 +10,7 @@ stock = st.text_input("Ex: $TESLA")
 
 tweets_list = []
 
-for i, tweet in enumerate(sntwitter.TwitterSearchScraper('$AMD since:2022-08-30 until:2022-08-31').get_items()):
+for i, tweet in enumerate(sntwitter.TwitterSearchScraper('{} since:2022-08-30 until:2022-08-31'.format(stock)).get_items()):
     tweets_list.append([tweet.date, tweet.content])
 
 st.write(len(tweets_list))
