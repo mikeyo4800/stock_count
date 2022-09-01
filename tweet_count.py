7a2e9df0d370a5f91d986d6950804d8c375453ac
mikeyo4800 <mikeyo4800@gmail.com>
2022-09-01 10:26:50 -0400

    updated dates

--- tweet_count.py
+++ tweet_count.py
@@ -6,11 +6,11 @@ import snscrape.modules.twitter as sntwitter
 st.title("Counts of Tweets by Stock Symbol")
 st.subheader("Enter stock symbol to return amount of mentions in the last week")
 
-stock = st.text_input("Ex: $TESLA")
+stock = st.text_input("Ex: $TSLA")
 
 tweets_list = []
 
-for i, tweet in enumerate(sntwitter.TwitterSearchScraper('{} since:2022-08-30 until:2022-08-31'.format(stock)).get_items()):
+for i, tweet in enumerate(sntwitter.TwitterSearchScraper('{} since:2022-08-31 until:2022-09-01'.format(stock)).get_items()):
     tweets_list.append([tweet.date, tweet.content])
 
 st.write(len(tweets_list))
